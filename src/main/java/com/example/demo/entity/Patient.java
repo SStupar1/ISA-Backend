83f484ee5633618274c568f8ecdabd8a6af1a3c6
SStupar2 <stefan.stupar2@gmail.com>
2020-08-21 16:50:34 +0200

    User models

---
+++ src/main/java/com/example/demo/entity/Patient.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+/*
+    @ManyToMany(mappedBy = "patients")
+    private Set<Clinic> clinics;
+*/
+    @ManyToMany(mappedBy = "patients")
+    private Set<MedicalStaff> medicals;
+/*
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "medical_record_id", referencedColumnName = "id")
+    private MedicalRecord medicalRecord;
+
+    @OneToMany(mappedBy = "patient",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    Set<MedicalExaminationReport> medicalExaminationReports = new HashSet<>();
+*/
+    private Boolean active;
+}
